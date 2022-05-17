6c22b479ae0f79fe91ffdd23aa422658f70cb174
Gareth Hutchins <Gareth.Hutchins@gmail.com>
2022-05-17 22:26:26 +0100

    Web Service and Graph Completed

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Magellan-Chrome-Summarizer
-A Chrome Extension to Summarize Webstie Using Opentext Magellan. You'll need your own Magellan instance for this, contact your Opentext Acount Manager.
-To point to your Magellan enviroment, modify the URL in popup.js
-In chrome, enter chrome://extensions/ in the address bar
-Select Load Unpacked from the top and there you have it
+# Narrative Flow Chrome Extension
+A Chrome extension to support the narrative flow analysis project for University of Reading.
+This extension can analyse a web page and graphically display its narrative flow.
+A screen shot is shown below:
+![Alt text](ScreenShot.png "Title")
--- popup.js
+++ popup.js
@@ -7,68 +7,117 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
     var NOT_SAFE_IN_XML_1_0 = /[^\x09\x0A\x0D\x20-\xFF\x85\xA0-\uD7FF\uE000-\uFDCF\uFDE0-\uFFFD]/gm;
     return input.replace(NOT_SAFE_IN_XML_1_0, '');
 };
-
-  function callMagellan(content,summary) {
-    var result = '';
-    //Clean the Content
-    var text = content.innerText;
-    text = cleanXmlChars(text);
-    text = text.replace(/[\n\r]+/g, ' ');
-    text = text.replace(/&/g,"&amp;");
-    text = text.replace(/</g,"&lt;");
-    text = text.replace(/>/g,"&gt;");
-    text = text.replace(/"/g,"&quot;");
-    text = text.replace(/'/g,"&apos;");
-    text = text.replace(/\[\d*\]/g, ' ');
-    text = text.replace( /\.(?=[^\d])[ ]*/g , '. ')
-    //Now create the request
-    var command = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Nserver><ResultEncoding>UTF-8</ResultEncoding><TextID>summarizer</TextID><NSTEIN_Text>";
-    command = command + text;
-    //Say we're looking for # sentences
-    command = command + "</NSTEIN_Text><LanguageID>ENGLISH</LanguageID><Methods><nsummarizer><NbSentences>2</NbSentences><KBid>IPTC</KBid></nsummarizer><NSentiment></NSentiment></Methods></Nserver>";
+function random_rgba() {
+  var o = Math.round, r = Math.random, s = 255;
+  return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+};
+function callSummariser(content,summary) {
+  var result = '';
+  //Clean the Content
+  var text = content.innerText;
+  text = cleanXmlChars(text);
+  text = text.replace(/[\n\r]+/g, ' ');
+  text = text.replace(/&/g,"&amp;");
+  text = text.replace(/</g,"&lt;");
+  text = text.replace(/>/g,"&gt;");
+  text = text.replace(/"/g,"&quot;");
+  text = text.replace(/'/g,"&apos;");
+  text = text.replace(/\[\d*\]/g, ' ');
+  text = text.replace( /\.(?=[^\d])[ ]*/g , '. ')
+  //Now create the request
+  var command = text;
+    
     //now do the post
-    var URL = 'http://otca-demo.eastus2.cloudapp.azure.com:40002/rs/';
+    var url = 'http://127.0.0.1:8000/process_text/';
     //var result = "";
-    fetch(URL, {
-      method: "POST",
-      body : command,
-      headers : {"Content-Type" : "application/xml"},
-    })
-    .then(function(res) {
-      if (res.ok) { // ok if status is 2xx
-        console.log('OK ' + res.statusText);
-      } else {
-        console.log('Request failed.  Returned status of ' + res.status);
+    var formdata = new FormData();
+    formdata.append("text",command);
+    formdata.append("model_id", "79b376ac-362b-40e1-b4c1-12c74a8883b3");
+    formdata.append("window_size", "255");
+    formdata.append("window_slide", "125");
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow'
+    };
+    fetch("http://127.0.0.1:8000/process_text/", requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      console.log('Response ',data)
+      //Now get the text values
+      var process_results = data.process_results
+      console.log('Process Results',process_results)
+      //Now loop through all of the results and build some dictionaries
+      
+      var topics = {}
+      var arrayLength = process_results.length;
+      var xValues = []
+      for (var i=0;i < arrayLength; i++){
+        //Now get the topics
+        //Add the window position
+        xValues.push(i)
+        //Get the first list of topics
+        var ts = process_results[i].Topics;
+        var cs = process_results[i].Scores;
+        console.log('ts',ts)
+        var tsl = ts.length;
+        for (var ct=0;ct < tsl;ct++) {
+          //see if it's in the dictionary
+          console.log('Current Topic',ts[ct])
+          if (ts[ct] in topics) {
+            //add the value
+            topics[ts[ct]].push([process_results[i].Scores[ct]]);
+          }
+          else {
+            //add it to the dictionary
+            topics[ts[ct]] = [process_results[i].Scores[ct]];
+          }
+        }
       }
-      return res.text()
-    })
-    .then(function(text) {
-      parser = new DOMParser();
-      var xmlDoc = parser.parseFromString(text,"text/xml");
-      result = xmlDoc.getElementsByTagName("Summary")[0].textContent;
-      console.log(result);
-      summary.innerText = result;
-      //var DocLevels = xmlDoc.getElementsByTagName("DocumentLevel")[0];
-      //Subjectivity.innerText = DocLevels.getElementsByTagName("Subjectivity")[0].textContent;
-      //tone.innerText = DocLevels.getElementsByTagName("Tone")[0].textContent;
-      //console.log('Subjectivity' + Subjectivity);
-      //console.log('Tone' + tone);
-      return result;
-    })
+      console.log('Topics', JSON.stringify(topics))
+      //var xValues = [50,60,70,80,90,100,110,120,130,140,150];
+      var yValues = [7,8,8,9,9,9,10,11,14,14,15];
+      //Now save the plot data to objects
+      var plotData = []
+      Object.entries(topics).forEach(([key,value]) => {
+        current_topic = {};
+        current_topic['label'] = key;
+        current_topic['fill'] = false;
+        current_topic['lineTenstion'] = 0;
+        current_topic['data'] = value;
+        var colour = random_rgba();
+        current_topic['backgroundColor'] = colour;
+        current_topic['borderColor'] = colour;
+        plotData.push(current_topic);
+      });
+      
+
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: plotData
+  },
+  options: {
+    legend: {display: true},
+    scales: {
+      yAxes: [{ticks: {min: 0, max:100}}],
+    }
   }
+});
+    })
+    .then(
+      result => console.log(result))
+    .catch(error => console.log('error', error));
+}
 
  
-  
   function onWindowLoad() {
   
     var message = document.querySelector('#message');
-    var sentences = document.querySelector('#demo');
-    var ranger = document.querySelector('#myRange');
+
     var summary = document.querySelector('#summary');
     
-    
-    //var subjectivity = document.querySelector('#subjectivity');
-    //var tone = document.querySelector('#tone');
     chrome.tabs.executeScript(null, {
       file: "getPagesSource.js"
     }, function() {
@@ -77,7 +126,7 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
         message.innerText = 'There was an error running the script : \n' + chrome.runtime.lastError.message;
       }
       else {
-        var magellanResult = callMagellan(message,summary);
+        var magellanResult = callSummariser(message,summary);
       }
     });
   }
